8846eace6238424b234a3b6d13452f7febdb6914
AlexWheelock <130195109+AlexWheelock@users.noreply.github.com>
2024-02-07 13:26:28 -0700

    init

---
+++ RollOfTheDice/RollOfTheDice.vb
@@ -0,0 +1,15 @@
+﻿'Alex Wheelock
+'RCET 0265
+'Spring 2024
+'Roll Of The Dice
+'https://github.com/AlexWheelock/RollOfTheDice.git
+
+Option Strict On
+Option Explicit On
+Module RollOfTheDice
+
+    Sub Main()
+
+    End Sub
+
+End Module
